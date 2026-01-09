fa5b4f82b1ae8a316a52c390a085a0e4e77f5d5c
Tumi-Ops <tumiso2001@gmail.com>
2026-01-09 17:30:26 +0200

    fixed terraform errors

--- terraform/cognito.tf
+++ terraform/cognito.tf
@@ -68,7 +68,7 @@ resource "aws_cognito_managed_login_branding" "example" {
   client_id    = aws_cognito_user_pool_client.FlightSyteClient.id
   user_pool_id = aws_cognito_user_pool.FlightSyteUserPool.id
   asset {
-    bytes      = filebase64("/pexels-pixabay-62623.jpg")
+    bytes      = filebase64("${path.module}/pexels-pixabay-62623.jpg")
     category   = "PAGE_BACKGROUND"
     color_mode = "DARK"
     extension  = "JPEG"
--- terraform/iam.tf
+++ terraform/iam.tf
@@ -393,39 +393,39 @@ resource "aws_iam_role_policy_attachment" "subscribe_lambda_logs" {
 
 # IAM Role and Policy for ECS Task Execution
 #######################################################################################################################################
-resource "aws_iam_role" "ecs_execution" {
-  name = "ecsTaskExecutionRole"
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "flightsyte-ecsTaskExecutionRole"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Effect = "Allow"
+      Effect    = "Allow"
       Principal = { Service = "ecs-tasks.amazonaws.com" }
-      Action = "sts:AssumeRole"
+      Action    = "sts:AssumeRole"
     }]
   })
 }
 
-resource "aws_iam_role" "ecs_task" {
+resource "aws_iam_role" "ecs_task_role" {
   name = "ecsTaskRole"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Effect = "Allow"
+      Effect    = "Allow"
       Principal = { Service = "ecs-tasks.amazonaws.com" }
-      Action = "sts:AssumeRole"
+      Action    = "sts:AssumeRole"
     }]
   })
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
-  role       = aws_iam_role.ecs_execution.name
+  role       = aws_iam_role.ecs_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 resource "aws_iam_role_policy_attachment" "ssm_read_policy" {
-  role       = aws_iam_role.ecs_task.name
+  role       = aws_iam_role.ecs_task_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
 }
 #######################################################################################################################################
