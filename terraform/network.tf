6463bb07c4242c82318e152c32573cc732e9099f
Tumi-Ops <tumiso2001@gmail.com>
2026-01-11 16:07:38 +0200

    fixed minor reference errors

--- terraform/network.tf
+++ terraform/network.tf
@@ -76,7 +76,7 @@ resource "aws_security_group" "flightsyte_alb_sg" {
   vpc_id      = aws_vpc.flightsyte_vpc.id
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_alb_tls_ipv4_http" {
+resource "aws_vpc_security_group_ingress_rule" "allow_alb_tls_ipv4" {
   security_group_id = aws_security_group.flightsyte_alb_sg.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
